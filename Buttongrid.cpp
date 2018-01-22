f5239687b209743d5ebe3ce80f91db3ef2e4f7db
Jose Rullan <jerullan@yahoo.com>
2018-01-21 22:56:49 -0400

    Implemented bitwise configuration options for highlighting, using labels and for operating in maintained or momentary buttons in Buttongrid.

--- Buttongrid.cpp
+++ Buttongrid.cpp
@@ -1,35 +1,46 @@
 #include "Buttongrid.h"
 
+/*
 Buttongrid::Buttongrid(unsigned char size){
 	if(text = (char *)malloc(DISPLAY_SIZE+1)) memset(text,0,DISPLAY_SIZE+1); 
 	columns = size;
 	rows = size;
 	gridSize = size;
 
-	//Allocate memory for labels
+	//Allocate memory for labels and states
 	if(rows >= 0 && columns >= 0){
 		char qty = rows * columns;
 		if(labels = (const char**) malloc(qty * 8 * sizeof(char))){
 			memset(labels,0,qty*8*sizeof(char));
+		}
+		if(states = (unsigned char*)malloc(qty)){
+			memset(states,0,qty);
 		}
 	}
 	
 	this->init();
 }
+*/
 
-Buttongrid::Buttongrid(unsigned char r, unsigned char c){
+Buttongrid::Buttongrid(unsigned char r, unsigned char c, unsigned char cfg){
 	if(text = (char *)malloc(DISPLAY_SIZE+1)) memset(text,0,DISPLAY_SIZE+1);
 	columns = c;
 	rows = r;
+	this->config = cfg;
 	
-	//Allocate memory for labels
+	//Allocate memory for labels and states
+	
 	if(rows >= 0 && columns >= 0){
 		char qty = rows * columns;
-		if(labels = (const char**) malloc(qty * 8 * sizeof(char))){
-			memset(labels,0,qty*8*sizeof(char));
+		if(config&BUTTONGRID_USELABELS){
+			if(labels = (const char**) malloc(qty * 8 * sizeof(char))){
+				memset(labels,0,qty*8*sizeof(char));
+			}
+		}
+		if(states = (unsigned char*)malloc(qty)){
+			memset(states,0,qty);
 		}
 	}
-	
 	this->init();
 }
 
@@ -101,12 +112,9 @@ void Buttongrid::drawGrid(){
 	{
 	  for(byte c=1; c<=columns; c++)
 	  {
-		  //drawButtonId(getId(r,c),labels[r-1][c-1]);
 		  drawLabel(getId(r,c));
 	  }
 	}	
-	
-	
  	return;
 }
 
@@ -116,7 +124,7 @@ void Buttongrid::drawLabel(unsigned char id){
 	unsigned char length;
 	int xPos,yPos;
 	
-	if(labels[id] != 0){
+	if((config&BUTTONGRID_USELABELS) && (labels[id] != 0)){
 		length = getTextLength(labels[id]);
 	}else{
 		length = Tft.Get_Digits(id);
@@ -125,14 +133,13 @@ void Buttongrid::drawLabel(unsigned char id){
 	xPos = getCenterTextX(x+(colIndex*btnWidth)+borderWidth*(1+colIndex), btnWidth, length);
 	yPos = getCenterTextY(y+(rowIndex*btnHeight)+borderWidth*(1+rowIndex), btnHeight);
 
-	if(labels[id] != 0){
+	if((config&BUTTONGRID_USELABELS) && (labels[id] != 0)){
 		Tft.drawString(labels[id],xPos,yPos,fontSize,fgColor);
 	}else{
 		Tft.drawNumber(id,xPos,yPos,fontSize,fgColor);
 	}
 	return;
 }
-
 
 void Buttongrid::setEventHandler(void (*functionPointer)(Buttongrid *, unsigned char)){
 	eventHandler = functionPointer;
@@ -154,8 +161,10 @@ unsigned char Buttongrid::getId(unsigned char row, unsigned char column){
 	return val;
 }
 
-void Buttongrid::setName(unsigned char id, const char name[8]){
-	labels[id] = name;	
+void Buttongrid::setLabel(unsigned char id, const char name[8]){
+	if(config&BUTTONGRID_USELABELS){
+		labels[id] = name;
+	}		
 	return;
 }
 
@@ -170,7 +179,7 @@ bool Buttongrid::checkTouch(Point* p){
 	bool pressed = false;
 	if(lastMillis + debounceTime < millis()){ 
 		if((p->x > x+borderWidth) && (p->x < x+w-borderWidth) && (p->y > y+borderWidth) && (p->y < y+h-borderWidth)){
-			//num coordinates
+			//id coordinates
 			for(int r = 1;(r <= rows)&&(!pressed); r++)
 			{
 				// Determine the bounding y's for this row
@@ -183,31 +192,32 @@ bool Buttongrid::checkTouch(Point* p){
 					boundX1 = x + (btnWidth)*(c-1) + borderWidth*c;
 					boundX2 = boundX1 + btnWidth;
 					
-					int num = columns*(r - 1) + c;
+					int id = columns*(r - 1) + c;
 					if((p->x > boundX1) && (p->x < boundX2) && (p->y > boundY1) && (p->y < boundY2)){
-						// Restore last button pressed appearance
-						if(clearLastPressed && lastPressed != 0){
-							char cIndex = getColumn(lastPressed) - 1;
-							char rIndex = getRow(lastPressed) - 1;
-							int lastX = x + btnWidth*cIndex + borderWidth*(cIndex + 1);
-							int lastY = y + btnHeight*rIndex + borderWidth*(rIndex + 1);
-							if(HIGHLIGHT == 1){
-								Tft.fillRect(lastX,lastY,btnWidth,btnHeight,bgColor);
+						
+						if(config & BUTTONGRID_MAINTAINED){
+							states[id] = (states[id])?0:1;
+							if(config&BUTTONGRID_HIGHLIGHT){
+								Tft.fillRect(boundX1,boundY1,btnWidth,btnHeight,(states[id])?highlightColor:bgColor);
+								drawLabel(id);
 							}
-							drawLabel(lastPressed);
+						}else{
+							if ((config&BUTTONGRID_HIGHLIGHT) && (lastPressed != 0)){
+								// Restore last button pressed appearance
+								char cIndex = getColumn(lastPressed) - 1;
+								char rIndex = getRow(lastPressed) - 1;
+								int lastX = x + btnWidth*cIndex + borderWidth*(cIndex + 1);
+								int lastY = y + btnHeight*rIndex + borderWidth*(rIndex + 1);
+								Tft.fillRect(lastX,lastY,btnWidth,btnHeight,bgColor);
+								drawLabel(lastPressed);
+								Tft.fillRect(boundX1,boundY1,btnWidth,btnHeight,highlightColor);
+								drawLabel(id);
+							}
 						}
-						
-						// Highlight currently pressed button
-						if(HIGHLIGHT == 1){
-							Tft.fillRect(boundX1,boundY1,btnWidth,btnHeight,highlightColor);
-						}
-						
-						drawLabel(num);
-						
 						// Call event handler with pressed button id
-						eventHandler(this,num);
+						eventHandler(this,id);
 						pressed = true;
-						lastPressed = num;
+						lastPressed = id;
 					}
 				}// -- columns for loop
 			}// --rows for loop
--- Buttongrid.h
+++ Buttongrid.h
@@ -13,7 +13,9 @@
 #ifndef BUTTONGRID_H
 #define BUTTONGRID_H
 
-#define HIGHLIGHT 1
+#define BUTTONGRID_HIGHLIGHT 	0b0001
+#define BUTTONGRID_MAINTAINED 	0b0010
+#define BUTTONGRID_USELABELS	0b0100
 
 #include "Canvas.h"
 #include "Button.h"
@@ -22,8 +24,8 @@ class Buttongrid : public Button
 {
 
 public:
-	Buttongrid(unsigned char r, unsigned char c);
-	Buttongrid(unsigned char gridSize);
+	Buttongrid(unsigned char r, unsigned char c, unsigned char configuration);
+	//Buttongrid(unsigned char gridSize);
 	virtual ~Buttongrid();
 
 	// Attributes
@@ -32,17 +34,19 @@ public:
 	byte rows;
 	byte hGap = 0;
 	byte vGap = 0;
+	unsigned char config = 0;
 	int btnWidth,btnHeight;
 	unsigned char lastPressed = 0; //Last pressed button ID (number)
 	const char** labels;
+	unsigned char* states;
 	int highlightColor = YELLOW;
-	unsigned char clearLastPressed = 1; // used to decide if to redraw as a maintained or as a momentary
+	unsigned char maintained = 1; // used to decide if to redraw as a maintained or as a momentary
 	
-	// Methods
+	// Methods	
 	void init();
 	void setSize(int width, int height);
 	void configure(byte gridSize);
-	void setName(unsigned char id, const char name[8]);
+	void setLabel(unsigned char id, const char name[8]);
 	unsigned char getColumn(unsigned char number);
 	unsigned char getRow(unsigned char number);
 	unsigned char getId(unsigned char row, unsigned char col);
