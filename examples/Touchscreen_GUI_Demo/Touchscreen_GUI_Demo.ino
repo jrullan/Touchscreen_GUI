164ca20d6aeb01c7249947ad10954afdae77f3e9
Jose Rullan <jerullan@yahoo.com>
2018-09-15 22:15:36 -0400

    Updated Demo

--- examples/Touchscreen_GUI_Demo/Touchscreen_GUI_Demo.ino
+++ examples/Touchscreen_GUI_Demo/Touchscreen_GUI_Demo.ino
@@ -22,9 +22,9 @@
 
 // Use either of these three and 
 // its corresponding class
-#include <Canvas_SEEEDTOUCH.h>
+//#include <Canvas_SEEEDTOUCH.h>
 //#include <Canvas_FT6206.h>
-//#include <Canvas_STMPE610.h>
+#include <Canvas_STMPE610.h>
 
 #include <Screen.h>
 #include <Terminal.h>
@@ -32,49 +32,77 @@
 #include <Button.h>
 #include <Dial.h>
 #include <Numkey.h>
+#include <IconButton.h>
+#include <icons.h>
 
 // Use either of these three and
 // its corresponding include above
-Canvas_SEEEDTOUCH canvas = Canvas_SEEEDTOUCH(TFT_PORTRAIT,BLACK);
+//Canvas_SEEEDTOUCH canvas = Canvas_SEEEDTOUCH(TFT_PORTRAIT,BLACK);
 //Canvas_FT6206 canvas = Canvas_FT6206(TFT_PORTRAIT,BLACK);
-//Canvas_STMPE610 canvas = Canvas_STMPE610(TFT_PORTRAIT,BLACK);
+Canvas_STMPE610 canvas = Canvas_STMPE610(TFT_PORTRAIT,BLACK);
 
-Display header = Display(240,50,BLUE,WHITE,WHITE,20);
+Display header = Display(240,40,BLUE,WHITE,WHITE,20);
 Display title = Display(20);
-Terminal terminal = Terminal(240,50);
+Terminal terminal = Terminal(240,50,TERMINAL_SCROLL_UP);
 Numkey numkey = Numkey();
-
-Screen screen_main = Screen(&canvas);
 Button btnMain = Button();
-Button btnClearTerminal = Button();
-
-Screen screen_dial = Screen(&canvas);
 Button btnDial = Button();
+Button btnButtons = Button();
+
+Screen screen_main = Screen(&canvas,0,40,240,190);
+Screen screen_dial = Screen(&canvas,0,40,240,190);
+Screen screen_buttons = Screen(&canvas,0,40,240,190);
+
 Dial dial = Dial();
 Button btnPlus = Button(20,GRAY1,WHITE,ILI9341_LIGHTGREY);  //Initialization version for round buttons
 Button btnMinus = Button(20,GRAY1,WHITE,ILI9341_LIGHTGREY); //Initialization version for round buttons
+
+IconButton btnBulb = IconButton(50,50,lightbulb_off,lightbulb_on);
+IconButton btnSlider = IconButton(60,30,slider_off,slider_on);
 
 const char increment = 1;
 
 //==================================
 // EVENT HANDLING ROUTINES
 //==================================
+
+void btnIconEventHandler(Button* btn){
+  if(btn == &btnBulb){
+    terminal.print((btn->touched)?"Bulb pressed":"Bulb unpressed",YELLOW);
+  }
+  if(btn == &btnSlider){
+    terminal.print((btn->touched)?"Slider On":"Slider Off",GREEN);
+  }
+  btn->update();
+}
+
 void btnMainEventHandler(Button* btn){
-  terminal.print("Switched to Main Screen");
   header.setText("Main");
   canvas.setScreen(&screen_main);
+  terminal.clear();
+  welcomeMessage();
 }
 
 void btnDialEventHandler(Button* btn){
   // Show the numkey to enter a password
   // The numkey event handler will verify the password
   // and change the screen if the password is correct
-  canvas.add(&numkey,60,50);
+  //canvas.add(&numkey,60,50);
+  header.setText("Dial");
+  canvas.setScreen(&screen_dial);
+  terminal.clear();
+  terminal.print("Dial reprents a value in a range");
+  terminal.print("Press the + and - buttons");
+  terminal.print("To change it's value");
 }
 
-void btnClearTerminalEventHandler(Button* btn){
+void btnButtonsEventHandler(Button* btn){
+  header.setText("Buttons");
+  canvas.setScreen(&screen_buttons);
   terminal.clear();
-  Serial.println("Terminal Cleared");
+  terminal.print("Icon buttons use images to");
+  terminal.print("represent the button state");
+  terminal.print("Press a button to see it's state");
 }
 
 void btnPlusEventHandler(Button* btn){
@@ -112,11 +140,16 @@ void numkeyEventHandler(Numkey* nk){
   nk->clear();
 }
 
+void welcomeMessage(){
+  terminal.print("Welcome to Touchscreen GUI demo",YELLOW);
+  terminal.print("This demo shows up some features",GREEN);
+  terminal.print("of the library. Enjoy!");  
+}
+
 //==================================
 // SETUP
 //==================================
-void setup() {
-  Serial.begin(115200);
+void guiSetup(){
   canvas.init();
 
   // ===== MAIN SCREEN =====
@@ -124,8 +157,8 @@ void setup() {
   title.setSize(240,40);
   title.setColors(screen_main.bgColor,GRAY2,screen_main.bgColor);
   title.borderWidth = 0;
-  title.setText("GUI DEMO");
-  screen_main.add(&title,0,120);
+  title.setText("GUI DEMO",false);
+  screen_main.add(&title,0,screen_main.h/2-FONT_Y*title.fontSize);
 
   // ===== SCREEN TWO =====
   btnPlus.setText("+");
@@ -146,16 +179,31 @@ void setup() {
   dial.setHiLimit(75,RED);
   dial.setSP(70,BLUE);
   dial.setLowLimit(70,BLUE);
-  dial.setCV(72);
+  dial.setCV(72,false);
 
   screen_dial.bgColor = screen_main.bgColor;//0xf80f;
-  screen_dial.add(&dial,100,130);
-  screen_dial.add(&btnPlus,160,80);
-  screen_dial.add(&btnMinus,160,140);
+  screen_dial.add(&dial,100,screen_dial.h/2);
+  screen_dial.add(&btnPlus,160,screen_dial.h/2 - dial.radius);
+  screen_dial.add(&btnMinus,160,screen_dial.h/2 + dial.radius - btnMinus.h);
+
+  // ===== SCREEN THREE - BUTTONS  =====  
+  screen_buttons.bgColor = 0xDFE;
+
+  btnBulb.setEventHandler(&btnIconEventHandler);
+  btnBulb.transparentColor = BLACK;
+  btnBulb.init();
+
+  btnSlider.setEventHandler(&btnIconEventHandler);
+  btnSlider.transparentColor = BLACK;
+  btnSlider.init();
+
+  screen_buttons.add(&btnBulb,95,50);
+  screen_buttons.add(&btnSlider,90,110);
 
   // ===== CANVAS GENERAL ITEMS  =====  
   btnMain.setSize(80,40);
   btnMain.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnMain.cornerRadius = 8;
   btnMain.setEventHandler(&btnMainEventHandler);
   btnMain.setText("Main");
   btnMain.setDebounce(200);
@@ -163,17 +211,19 @@ void setup() {
 
   btnDial.setSize(80,40);
   btnDial.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnDial.cornerRadius = 8;
   btnDial.setEventHandler(&btnDialEventHandler);
   btnDial.setText("Dial");
   btnDial.setDebounce(200);
   btnDial.init();
   
-  btnClearTerminal.setSize(80,40);
-  btnClearTerminal.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
-  btnClearTerminal.setEventHandler(&btnClearTerminalEventHandler);
-  btnClearTerminal.setText("Clear");
-  btnClearTerminal.setDebounce(200);
-  btnClearTerminal.init();
+  btnButtons.setSize(80,40);
+  btnButtons.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnButtons.cornerRadius = 8;
+  btnButtons.setEventHandler(&btnButtonsEventHandler);
+  btnButtons.setText("Btns");
+  btnButtons.setDebounce(200);
+  btnButtons.init();
 
   numkey.setSize(120,180);
   numkey.setColors(GRAY1,BLACK,WHITE);
@@ -181,15 +231,22 @@ void setup() {
   numkey.setNumkeyEventHandler(&numkeyEventHandler);
   numkey.setDebounce(100);
   
-  header.setText("Main Screen");
-  terminal.setColors(WHITE,GRAY2,GRAY1);
+  header.setText("Main Screen",false);
+  terminal.setColors(BLACK,WHITE,WHITE);
     
-  canvas.add(&header,0,0);
-  canvas.add(&btnMain,0,230);
-  canvas.add(&btnDial,80,230);
-  canvas.add(&btnClearTerminal,160,230);  
-  canvas.add(&terminal,0,270);
-  canvas.setScreen(&screen_main);
+  canvas.add(&header,0,0,false);
+  canvas.add(&btnMain,0,230,false);
+  canvas.add(&btnDial,80,230,false);
+  canvas.add(&btnButtons,160,230,false);  
+  canvas.add(&terminal,0,270,false);
+  canvas.setScreen(&screen_main);  
+
+  welcomeMessage();
+}
+
+void setup() {
+  Serial.begin(115200);
+  guiSetup();
 }
 
 //==================================
