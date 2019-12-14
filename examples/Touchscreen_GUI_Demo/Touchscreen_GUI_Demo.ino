de7a56dd2b56322a13daa73156d0708da304b409
Jose Rullan <jerullan@yahoo.com>
2019-12-13 23:31:48 -0400

    Updated example Touchscreen_GUI_Demo to make uniform background colors

--- examples/Touchscreen_GUI_Demo/Touchscreen_GUI_Demo.ino
+++ examples/Touchscreen_GUI_Demo/Touchscreen_GUI_Demo.ino
@@ -31,7 +31,8 @@
 #define TFT_DS 5  // esp32 MiniKit D8
 #define TS_CS 17  // esp32 MiniKit D3
 
-Canvas_XPT2046 canvas = Canvas_XPT2046(TFT_PORTRAIT,BLACK,TFT_CS,TFT_DS,TS_CS);
+#define BG_COLOR 0xDFE  //Background color (light blue)
+Canvas_XPT2046 canvas = Canvas_XPT2046(TFT_PORTRAIT,BG_COLOR,TFT_CS,TFT_DS,TS_CS);
 Display header = Display(240,40,BLUE,WHITE,WHITE,20);
 Display title = Display(20);
 Terminal terminal = Terminal(240,50,TERMINAL_SCROLL_UP);
@@ -55,179 +56,174 @@ const char increment = 1;
 //==================================
 
 void btnIconEventHandler(Button* btn){
-	if(btn == &btnBulb){
-		terminal.print((btn->touched)?"Bulb pressed":"Bulb unpressed",YELLOW);
-	}
-	if(btn == &btnSlider){
-		terminal.print((btn->touched)?"Slider On":"Slider Off",GREEN);
-	}
-	btn->update();
+  if(btn == &btnBulb){
+    terminal.print((btn->touched)?"Bulb pressed":"Bulb unpressed",YELLOW);
+  }
+  if(btn == &btnSlider){
+    terminal.print((btn->touched)?"Slider On":"Slider Off",GREEN);
+  }
+  btn->update();
 }
 
 void btnMainEventHandler(Button* btn){
-	header.setText("Main");
-	canvas.setScreen(&screen_main);
-	terminal.clear();
-	welcomeMessage();
+  header.setText("Main");
+  canvas.setScreen(&screen_main);
+  terminal.clear();
+  welcomeMessage();
 }
 
 void btnDialEventHandler(Button* btn){
-	// Show the numkey to enter a password
-	// The numkey event handler will verify the password
-	// and change the screen if the password is correct
-	canvas.add(&numkey,60,50);
-	/*
-	header.setText("Dial");
-	canvas.setScreen(&screen_dial);
-	terminal.clear();
-	terminal.print("Dial reprents a value in a range");
-	terminal.print("Press the + and - buttons");
-	terminal.print("To change it's value");
-  */
+  // Show the numkey to enter a password
+  // The numkey event handler will verify the password
+  // and change the screen if the password is correct
+  terminal.clear();
+  terminal.print("Enter the numeric password:");
+  terminal.print("1234",GREEN);
+  canvas.add(&numkey,60,45);
 }
 
 void btnButtonsEventHandler(Button* btn){
-	header.setText("Buttons");
-	canvas.setScreen(&screen_buttons);
-	terminal.clear();
-	terminal.print("Icon buttons use images to");
-	terminal.print("represent the button state");
-	terminal.print("Press a button to see it's state");
+  header.setText("Buttons");
+  canvas.setScreen(&screen_buttons);
+  terminal.clear();
+  terminal.print("Icon buttons use images to");
+  terminal.print("represent the button state");
+  terminal.print("Press a button to see it's state");
 }
 
 void btnPlusEventHandler(Button* btn){
-	if(dial.getCV() >= dial.scaleMax){
-		terminal.print("Maximum value reached",RED);
-		return;
-	}
-	dial.setCV(dial.getCV()+increment);
+  if(dial.getCV() >= dial.scaleMax){
+    terminal.print("Maximum value reached",RED);
+    return;
+  }
+  dial.setCV(dial.getCV()+increment);
 }
 
 void btnMinusEventHandler(Button* btn){
-	if(dial.getCV() <= dial.scaleMin){
-		terminal.print("Minimum value reached",BLUE);
-		return;
-	}
-	dial.setCV(dial.getCV()-increment);
+  if(dial.getCV() <= dial.scaleMin){
+    terminal.print("Minimum value reached",BLUE);
+    return;
+  }
+  dial.setCV(dial.getCV()-increment);
 }
 
 void numkeyEventHandler(Numkey* nk){
-	char* password = "1234";
-	if(nk->getTextSize() == Widget::getTextLength(password)){
-		bool match = true;
-		Serial.println(nk->getText());
-		for(int i=0;i<nk->getTextSize();i++){
-			if(nk->getText()[i] != password[i]) match = false; 
-		}
-		if(match){
-			header.setText("Dial");
-			canvas.setScreen(&screen_dial);
-			//terminal.print("Password OK",GREEN);
-			terminal.clear();
-			terminal.print("Dial reprents a value in a range");
-			terminal.print("Press the + and - buttons");
-			terminal.print("To change it's value");
-			nk->clear();
-			return;
-		}
-	}
-	terminal.print("Wrong password entered",RED);
-	nk->clear();
+  char* password = "1234";
+  if(nk->getTextSize() == Widget::getTextLength(password)){
+    bool match = true;
+    Serial.println(nk->getText());
+    for(int i=0;i<nk->getTextSize();i++){
+      if(nk->getText()[i] != password[i]) match = false; 
+    }
+    if(match){
+      header.setText("Dial");
+      canvas.setScreen(&screen_dial);
+      terminal.clear();
+      terminal.print("Dial reprents a value in a range");
+      terminal.print("Press the + and - buttons");
+      terminal.print("To change it's value");
+      nk->clear();
+      return;
+    }
+  }
+  terminal.print("Wrong password entered",RED);
+  nk->clear();
 }
 
 void welcomeMessage(){
-	terminal.print("Welcome to Touchscreen GUI demo",YELLOW);
-	terminal.print("This demo shows up some features",GREEN);
-	terminal.print("of the library. Enjoy!");  
+  terminal.print("Welcome to Touchscreen GUI demo",YELLOW);
+  terminal.print("This demo shows up some features",GREEN);
+  terminal.print("of the library. Enjoy!");  
 }
 
 //==================================
 // SETUP
 //==================================
 void guiSetup(){
-	// ===== MAIN SCREEN =====
-	screen_main.bgColor = 0xDFE;
-	title.setSize(240,40);
-	title.setColors(screen_main.bgColor,GRAY2,screen_main.bgColor);
-	title.borderWidth = 0;
-	title.setText("GUI DEMO",false);
-	screen_main.add(&title,0,screen_main.h/2-FONT_Y*title.fontSize);
+  // ===== MAIN SCREEN =====
+  screen_main.bgColor = BG_COLOR;
+  title.setSize(240,40);
+  title.setColors(screen_main.bgColor,GRAY2,screen_main.bgColor);
+  title.borderWidth = 0;
+  title.setText("GUI DEMO",false);
+  screen_main.add(&title,0,screen_main.h/2-FONT_Y*title.fontSize);
 
-	// ===== SCREEN TWO =====
-	btnPlus.setText("+");
-	btnPlus.setEventHandler(&btnPlusEventHandler);
-	btnPlus.setDebounce(25);  
+  // ===== SCREEN TWO =====
+  screen_dial.bgColor = BG_COLOR;
 
-	btnMinus.setText("-");
-	btnMinus.setEventHandler(&btnMinusEventHandler);
-	btnMinus.setDebounce(25);  
+  btnPlus.setText("+");
+  btnPlus.setEventHandler(&btnPlusEventHandler);
+  btnPlus.setDebounce(25);  
 
-	dial.init();
-	dial.setSize(50);
-	dial.borderWidth = 5;
-	dial.setColors(WHITE,GRAY2,GRAY2);
-	dial.setLimits(60,70,90);
-	dial.setHiLimit(75,RED);
-	dial.setSP(70,BLUE);
-	dial.setLowLimit(70,BLUE);
-	dial.setCV(72,false);
+  btnMinus.setText("-");
+  btnMinus.setEventHandler(&btnMinusEventHandler);
+  btnMinus.setDebounce(25);  
 
-	screen_dial.bgColor = screen_main.bgColor;//0xf80f;
-	screen_dial.add(&dial,100,screen_dial.h/2);
-	screen_dial.add(&btnPlus,160,screen_dial.h/2 - dial.radius);
-	screen_dial.add(&btnMinus,160,screen_dial.h/2 + dial.radius - btnMinus.h);
+  dial.init();
+  dial.setSize(50);
+  dial.borderWidth = 5;
+  dial.setColors(WHITE,GRAY2,GRAY2);
+  dial.setLimits(60,70,90);
+  dial.setHiLimit(75,RED);
+  dial.setSP(70,BLUE);
+  dial.setLowLimit(70,BLUE);
+  dial.setCV(72,false);
 
-	// ===== SCREEN THREE - BUTTONS  =====  
-	screen_buttons.bgColor = 0xDFE;
+  screen_dial.add(&dial,100,screen_dial.h/2);
+  screen_dial.add(&btnPlus,160,screen_dial.h/2 - dial.radius);
+  screen_dial.add(&btnMinus,160,screen_dial.h/2 + dial.radius - btnMinus.h);
 
-	btnBulb.setEventHandler(&btnIconEventHandler);
-	btnBulb.transparentColor = BLACK;
+  // ===== SCREEN THREE - BUTTONS  =====  
+  screen_buttons.bgColor = BG_COLOR;
 
-	btnSlider.setEventHandler(&btnIconEventHandler);
-	btnSlider.transparentColor = BLACK;
+  btnBulb.setEventHandler(&btnIconEventHandler);
+  btnBulb.transparentColor = BLACK;
 
-	screen_buttons.add(&btnBulb,95,50);
-	screen_buttons.add(&btnSlider,90,110);
+  btnSlider.setEventHandler(&btnIconEventHandler);
+  btnSlider.transparentColor = BLACK;
 
-	// ===== CANVAS GENERAL ITEMS  =====  
-	btnMain.setSize(80,40);
-	btnMain.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
-	btnMain.cornerRadius = 8;
-	btnMain.setEventHandler(&btnMainEventHandler);
-	btnMain.setText("Main");
-	btnMain.setDebounce(200);
+  screen_buttons.add(&btnBulb,95,50);
+  screen_buttons.add(&btnSlider,90,110);
 
-	btnDial.setSize(80,40);
-	btnDial.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
-	btnDial.cornerRadius = 8;
-	btnDial.setEventHandler(&btnDialEventHandler);
-	btnDial.setText("Dial");
-	btnDial.setDebounce(200);
+  // ===== CANVAS GENERAL ITEMS  =====  
+  btnMain.setSize(80,40);
+  btnMain.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnMain.cornerRadius = 8;
+  btnMain.setEventHandler(&btnMainEventHandler);
+  btnMain.setText("Main");
+  btnMain.setDebounce(200);
 
-	btnButtons.setSize(80,40);
-	btnButtons.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
-	btnButtons.cornerRadius = 8;
-	btnButtons.setEventHandler(&btnButtonsEventHandler);
-	btnButtons.setText("Btns");
-	btnButtons.setDebounce(200);
+  btnDial.setSize(80,40);
+  btnDial.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnDial.cornerRadius = 8;
+  btnDial.setEventHandler(&btnDialEventHandler);
+  btnDial.setText("Dial");
+  btnDial.setDebounce(200);
 
-	numkey.setSize(120,180);
-	numkey.setColors(GRAY1,BLACK,WHITE);
-	numkey.init();
-	numkey.setEventHandler(&numkeyEventHandler);
-	numkey.setDebounce(100);
+  btnButtons.setSize(80,40);
+  btnButtons.setColors(GRAY1,WHITE,ILI9341_LIGHTGREY);
+  btnButtons.cornerRadius = 8;
+  btnButtons.setEventHandler(&btnButtonsEventHandler);
+  btnButtons.setText("Btns");
+  btnButtons.setDebounce(200);
 
-	header.setText("Main Screen",false);
-	terminal.setColors(BLACK,WHITE,WHITE);
+  numkey.setSize(120,180);
+  numkey.setColors(GRAY1,BLACK,WHITE);
+  numkey.init();
+  numkey.setEventHandler(&numkeyEventHandler);
+  numkey.setDebounce(100);
 
-	canvas.add(&header,0,0);
-	canvas.add(&btnMain,0,230);
-	canvas.add(&btnDial,80,230);
-	canvas.add(&btnButtons,160,230);  
-	canvas.add(&terminal,0,270);
-	canvas.setScreen(&screen_main);  
+  header.setText("Main Screen",false);
+  terminal.setColors(BLACK,WHITE,WHITE);
 
-	welcomeMessage();
+  canvas.add(&header,0,0);
+  canvas.add(&btnMain,0,230);
+  canvas.add(&btnDial,80,230);
+  canvas.add(&btnButtons,160,230);  
+  canvas.add(&terminal,0,270);
+  canvas.setScreen(&screen_main);  
+
+  welcomeMessage();
 }
 
 void setup() {
